88bacd5f0079199eac84f983db2367152dd63cca
Risang Radityatama <161427850+rxceed@users.noreply.github.com>
2025-12-27 22:38:46 +0700

    Update API URLs to use 'http://host'

---
+++ src/reeman_api_service/include/reeman_api_service/secret.hpp
@@ -0,0 +1,13 @@
+#ifndef SECRET_H
+#define SECRET_H
+
+#define POST_NAV_URL "http://host/cmd/nav"
+#define GET_POSE_URL "http://host/reeman/pose"
+#define GET_SPEED_URL "http://host/reeman/speed"
+#define POST_MAX_SPEED_URL "http://host/cmd/max_speed"
+#define POST_SPEED_URL "http://host/cmd/speed"
+#define POST_CANCEL_NAV_URL "http://host/cmd/cancel_goal"
+#define POST_NAV_POINT_URL "http://host/cmd/nav_name"
+#define GET_NAV_STATUS_URL "http://host/reeman/nav_status"
+
+#endif
